30d1ca632924536f15cfd5d1fcb138a7ccd3d065
Ronald Dijks <ronalddijks@protonmail.com>
2017-09-14 14:30:45 +0200

    ahduw

--- Startup.cs
+++ Startup.cs
@@ -1,47 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Threading;
-using Microsoft.AspNetCore.Antiforgery;
-using Microsoft.AspNetCore.Builder;
+
+
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using NLog.Extensions.Logging;
-using NLog.Web;
-using SimpleModelsAndRelations.Data;
-using SimpleModelsAndRelations.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SimpleModelsAndRelations
 {
-
-  static class StaticMailer {
-    public static MailOptions _mailOptions = new MailOptions();
-  }
-
-  public class MailOptions
-  {
-    public MailOptions()
-    {
-    }
-    public string MailApiToken { get; set; }
-    public string MailFrom { get; set; }
-  }
-
-  public class ApiOptions
-  {
-    public ApiOptions()
-    {
-    }
-    public string ApiToken { get; set; }
-  }
-
   public class Startup
   {
     public Startup(IHostingEnvironment env)
@@ -60,9 +24,6 @@ namespace SimpleModelsAndRelations
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure()
     { 
-      app.UseMvc(routes =>
-      {
-      }); 
     }
   }
 }
